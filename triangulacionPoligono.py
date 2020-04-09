3faf02cf9a259cf1daac2de18d67e8e302e2aa81
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-04-09 18:17:34 +0200

    Clase Arista

--- triangulacionPoligono.py
+++ triangulacionPoligono.py
@@ -2,13 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+TAMANNO_PUNTOS = 15
+MAX_COORDS = 10
+NUM_PUNTOS = 50
 
-class Punto:
+
+class MiPunto:
 
     def __init__(self, coordX, coordY):
         self.coords = np.array([coordX, coordY])
         self.quitado = False
-    #fin __init__
 
     def __str__(self):
         cadena = "Coordenadas:" + str(self.coords)
@@ -33,13 +36,76 @@ class Punto:
     def esDistintoDe(self, otro):
         return self.coords[0] != otro.coords[0] or self.coords[1] != otro.coords[1]
 
+    #Dice si el punto c está a la derecha de la recta que pasa por este punto y b
+    def estaALaDerecha(self, b, c):
+        vectorAB = self.calcularVectorCon(b)
+        vectorAC = self.calcularVectorCon(c)
+        matriz = np.array([vectorAB.tolist(), vectorAC.tolist()])
+        matrizBase = np.transpose(matriz)
+        det = np.linalg.det(matrizBase)
+        return det < 0
+
+    #Dice si el punto está por encima de b
+    def estaPorEncimaDe(self, b):
+        return self.getCoordY() > b.getCoordY()
+
 
 class Arista:
-    def __init__(self, inicio: Punto, fin: Punto):
+    def __init__(self, inicio: MiPunto, fin: MiPunto):
         self.inicio = inicio
         self.fin = fin
     #fin __init__
 
 
+class Vertice:
+    def __init__(self, punto: MiPunto, adyacenteIzq: MiPunto, adyacenteDer: MiPunto):
+        self.punto = punto
+        self.adyacenteIzq = adyacenteIzq
+        self.adyacenteDer = adyacenteDer
+        self.tipo = self.definirTipo()
+
+    def definirTipo(self):
+
+        return "Tipo1"
+
+
+def pintarPoligono(poligono):
+    plt.xlim(0, MAX_COORDS)
+    plt.ylim(0, MAX_COORDS)
+    x=[]
+    y=[]
+    for p in poligono:
+        x.append(p.getCoordX())
+        y.append(p.getCoordY())
+    x.append(x[0])
+    y.append(y[0])
+    plt.scatter(x, y, s=TAMANNO_PUNTOS)
+    plt.plot(x, y)
+    plt.show()
+
+
+def crearPoligono():
+    p1 = MiPunto(1, 2)
+    p2 = MiPunto(2, 1)
+    p3 = MiPunto(3, 2)
+    p4 = MiPunto(3, 4)
+    p5 = MiPunto(5, 2)
+    p6 = MiPunto(6, 4)
+    p7 = MiPunto(5, 3)
+    p8 = MiPunto(4, 7)
+    p9 = MiPunto(2, 8)
+    p10 = MiPunto(3, 6)
+    p11 = MiPunto(1, 7)
+    p12 = MiPunto(2, 3)
+    p13 = MiPunto(1, 2)
+
+    poligono = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13]
+
+    return poligono
+
+
 if __name__ == "__main__":
     print("ola")
+    poligono = crearPoligono()
+    pintarPoligono(poligono)
+
